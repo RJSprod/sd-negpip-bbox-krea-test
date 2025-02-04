bb5f495f9803a5e0eb313b2566ce2ec9786a4857
Haoming <hmstudy02@gmail.com>
2025-02-04 14:02:35 +0800

    update
    fix for XL (de42b74)

--- scripts/negpip.py
+++ scripts/negpip.py
@@ -2,7 +2,6 @@ import gradio as gr
 import torch
 import re
 import json
-import ldm.modules.attention as atm
 import modules.ui
 import modules
 from modules import prompt_parser, devices
@@ -10,15 +9,8 @@ from modules import prompt_parser, devices
 
 from modules import shared
 from modules.script_callbacks import CFGDenoiserParams, on_cfg_denoiser, on_ui_settings
+from ldm_patched.ldm.modules.attention import default
 
-debug = False
-debug_p = False
-
-try:
-    from ldm_patched.modules import model_management
-    forge = True
-except:
-    forge = False
 
 OPT_ACT = "negpip_active"
 OPT_HIDE = "negpip_hide"
@@ -103,7 +95,7 @@ class Script(modules.scripts.Script):
         elif not active: return
 
         self.rpscript = None
-        #get infomation of regponal prompter
+        # get information of regponal prompter
         from modules.scripts import scripts_txt2img
         for script in scripts_txt2img.alwayson_scripts:
             if "rp.py" in script.filename:
@@ -113,10 +105,8 @@ class Script(modules.scripts.Script):
 
         self.active = active
         self.batch = p.batch_size
-        self.isxl = hasattr(shared.sd_model,"conditioner")
-
-        self.rev = p.sampler_name in ["DDIM", "PLMS", "UniPC"]
-        if forge: self.rev = not self.rev
+        self.isxl = p.sd_model.is_sdxl
+        self.rev = p.sampler_name not in ["DDIM", "PLMS", "UniPC"]
 
         tokenizer = shared.sd_model.conditioner.embedders[0].tokenize_line if self.isxl else shared.sd_model.cond_stage_model.tokenize_line
 
@@ -124,7 +114,7 @@ class Script(modules.scripts.Script):
         def getshedulednegs(scheduled,prompts):
             output = []
             nonlocal flag
-            for i, batch_shedule in enumerate(scheduled):
+            for i, batch_schedule in enumerate(scheduled):
                 stepout = []
                 seps = None
                 if self.rpscript:
@@ -132,9 +122,9 @@ class Script(modules.scripts.Script):
                         seps = self.rpscript.seps
                     self.enable_rp_latent = seps == "AND"
 
-                for step,prompt in batch_shedule:
+                for step,prompt in batch_schedule:
                     sep_prompts = prompt.split(seps) if seps else [prompt]
-                    padd = 0
+                    pad = 0
                     padtextweight = []
                     for sep_prompt in sep_prompts:
                         minusmatches = re.finditer(minusgetter, sep_prompt)
@@ -145,16 +135,15 @@ class Script(modules.scripts.Script):
 
                             prompts[i] = prompts[i].replace(minusmatch.group(),"")
                         minus_targets = [x.split(":") for x in minus_targets]
-                        #print(minus_targets)
                         for text,weight in minus_targets:
                             weight = float(weight)
                             if text == "BREAK": continue
                             if weight < 0:
                                 textweights.append([text,weight])
                                 flag = True
-                        padtextweight.append([padd,textweights])
+                        padtextweight.append([pad,textweights])
                         tokens, tokensnum = tokenizer(sep_prompt)
-                        padd = tokensnum // 75 + 1 + padd
+                        pad = tokensnum // 75 + 1 + pad
                     stepout.append([step,padtextweight])
                 output.append(stepout)
             return output
@@ -184,17 +173,8 @@ class Script(modules.scripts.Script):
                 token, tokenlen = tokenizer(target[0])
                 conds.append(cond[0][0].cond[1:tokenlen +2,:] if not self.isxl else cond[0][0].cond["crossattn"][1:tokenlen +2,:] )
             conds = torch.cat(conds, 0)
-
-            conds = torch.split(conds, 75, dim=0)
-            condsout = []
-            condcount = []
-            for cond in conds:
-                condcount.append(cond.shape[0])
-                repeat = 0 if cond.shape[0] == 75 else 75 - cond.shape[0]
-                cond = torch.cat((start,cond,end.repeat(repeat + 1,1)),0)
-                condsout.append(cond)
-            condout = torch.cat(condsout,0).unsqueeze(0)
-            return condout.repeat(self.batch,1,1), condcount
+            conds = conds.unsqueeze(0)
+            return conds.repeat(self.batch,1,1), conds.shape[1]
 
         def calcconds(targetlist):
             outconds = []
@@ -218,9 +198,6 @@ class Script(modules.scripts.Script):
         if self.hrp: self.hr_conds_all = calcconds(hr_nip)
         if self.hrn: self.hr_unconds_all = calcconds(hr_pin)
 
-        #print(self.conds_all)
-        #print(self.unconds_all)
-
         resetpcache(p)
 
         def calcsets(A, B):
@@ -237,7 +214,7 @@ class Script(modules.scripts.Script):
         if not hasattr(self,"negpip_dr_callbacks"):
             self.negpip_dr_callbacks = on_cfg_denoiser(self.denoiser_callback)
 
-        #disable hookforward if hookfoward in regional prompter is eanble.
+        #disable hookforward if hookfoward in regional prompter is enable.
         #negpip operation is treated in regional prompter
 
         already_hooked = False
@@ -258,7 +235,6 @@ class Script(modules.scripts.Script):
         self.unconds_all = None
 
     def denoiser_callback(self, params: CFGDenoiserParams):
-        if debug: print(params.text_cond.shape)
         if self.active:
             if self.x is None: self.x = params.x.shape
             if self.x != params.x.shape: self.hr = True
@@ -274,7 +250,6 @@ class Script(modules.scripts.Script):
                         for region, conds, tokens in regions:
                             condslist.append(conds)
                             tokenslist.append(tokens)
-                            if debug: print(f"current:{params.sampling_step + 2},selected:{step}")
                         break
                 self.conds = condslist
                 self.contokens = tokenslist
@@ -293,22 +268,17 @@ class Script(modules.scripts.Script):
                 self.unconds = uncondslist
                 self.untokens = untokenslist
 
-from pprint import pprint
-
 def unload(self,p):
     if hasattr(self,"handle"):
         hook_forwards(self, p.sd_model.model.diffusion_model, remove=True)
         del self.handle
 
 def hook_forward(self, module):
-    def forward(x, context=None, mask=None, additional_tokens=None, n_times_crossframe_attn_in_self=0, value = None, transformer_options=None):
-        if debug: print(" x.shape:",x.shape,"context.shape:",context.shape,"self.contokens",self.contokens,"self.untokens",self.untokens)
+    def forward(x, context=None, mask=None, value=None, additional_tokens=None, *args, **kwargs):
 
-        def sub_forward(x, context, mask, additional_tokens, n_times_crossframe_attn_in_self,conds,contokens,unconds,untokens, latent = None):
-            if debug: print(" x.shape[0]:",x.shape[0],"batch:",self.batch *2)
+        def sub_forward(x, context, mask, additional_tokens, conds,contokens,unconds,untokens, latent = None):
 
             if x.shape[0] == self.batch *2:
-                if debug: print(" x.shape[0] == self.batch *2")
 
                 if self.rev:
                     contn,contp = context.chunk(2)
@@ -325,14 +295,14 @@ def hook_forward(self, module):
                     if contn.shape[0] != unconds.shape[0]:
                         unconds = unconds.expand(contn.shape[0],-1,-1)
                     contn =  torch.cat((contn,unconds),1)
-                xp = main_foward(self, module, ixp,contp,mask,additional_tokens,n_times_crossframe_attn_in_self,contokens)
-                xn = main_foward(self, module, ixn,contn,mask,additional_tokens,n_times_crossframe_attn_in_self,untokens)
+
+                xp = main_forward(self, module, ixp,contp,value,mask,additional_tokens,contokens,args,kwargs)
+                xn = main_forward(self, module, ixn,contn,value,mask,additional_tokens,untokens,args,kwargs)
 
                 out = torch.cat([xn,xp]) if self.rev else torch.cat([xp,xn])
                 return out
 
             elif latent is not None:
-                if debug:print(" latent is not None")
                 if latent:
                     conds = conds if conds is not None else None
                 else:
@@ -344,17 +314,12 @@ def hook_forward(self, module):
 
                 tokens = contokens if contokens is not None else untokens
 
-                out = main_foward(self, module, x,context,mask,additional_tokens,n_times_crossframe_attn_in_self,tokens)
+                out = main_forward(self, module, x,context,value,mask,additional_tokens,tokens,args,kwargs)
                 return out
 
             else:
-                if debug:
-                    print(" Else")
-                    print(context.shape[1] , self.conlen,self.unlen)
-
                 tokens = []
                 concon = counter(self.isxl)
-                if debug: print(concon)
                 if context.shape[1] == self.conlen * 77 and concon:
                     if conds is not None:
                         if context.shape[0] != conds.shape[0]:
@@ -367,79 +332,75 @@ def hook_forward(self, module):
                             unconds = unconds.expand(context.shape[0],-1,-1)
                         context = torch.cat([context,unconds],1)
                         tokens = untokens
-                out = main_foward(self, module, x,context,mask,additional_tokens,n_times_crossframe_attn_in_self,tokens)
+                out = main_forward(self, module, x,context,value,mask,additional_tokens,tokens,args,kwargs)
                 return out
 
         if self.enable_rp_latent:
             if len(self.conds) - 1 >= self.latenti:
-                out = sub_forward(x, context, mask, additional_tokens, n_times_crossframe_attn_in_self,self.conds[self.latenti],self.contokens[self.latenti],None,None ,latent = True)
+                out = sub_forward(x, context, mask, additional_tokens, self.conds[self.latenti],self.contokens[self.latenti],None,None ,latent = True)
                 self.latenti += 1
             else:
-                out = sub_forward(x, context, mask, additional_tokens, n_times_crossframe_attn_in_self,None,None,self.unconds[0],self.untokens[0], latent = False)
+                out = sub_forward(x, context, mask, additional_tokens, None,None,self.unconds[0],self.untokens[0], latent = False)
                 self.latenti = 0
             return out
         else:
             if self.conds is not None and self.unconds is not None and len(self.conds) > 0 and len(self.unconds) > 0:
-                return sub_forward(x, context, mask, additional_tokens, n_times_crossframe_attn_in_self,self.conds[0],self.contokens[0],self.unconds[0],self.untokens[0])
+                return sub_forward(x, context, mask, additional_tokens, self.conds[0],self.contokens[0],self.unconds[0],self.untokens[0])
             else:
-                return sub_forward(x, context, mask, additional_tokens, n_times_crossframe_attn_in_self,None,None,None,None)
+                return sub_forward(x, context, mask, additional_tokens, None,None,None,None)
 
     return forward
 
 count = 0
-pn = True
+p = True
 
 def counter(isxl):
-    global count, pn
+    global count, p
     count += 1
 
     limit = 70 if isxl else 16
-    outpn = pn
+    outpn = p
 
     if count == limit:
-        pn = not pn
+        p = not p
         count = 0
     return outpn
 
-def main_foward(self, module, x, context, mask, additional_tokens, n_times_crossframe_attn_in_self, tokens):
-    h = module.heads
-    context = context.to(x.dtype)
-    q = module.to_q(x)
+def main_forward(self, attn, x, context, value = None ,mask = None, temb = None, tokens = [], args = None, kwargs = None):
+        q = attn.to_q(x)
+        context = context.to(x.dtype)
+        context = default(context, x)
+        k = attn.to_k(context)
+        if value is not None:
+            v = attn.to_v(value)
+            del value
+        else:
+            v = attn.to_v(context)
 
-    context = atm.default(context, x)
-    k = module.to_k(context)
-    v = module.to_v(context)
-    if debug: print(h,context.shape,q.shape,k.shape,v.shape)
+        if self.active:
+            if tokens:
+                v[:,-tokens:,:] = -v[:,-tokens:,:]
 
-    _, _, dim_head = q.shape
-    dim_head //= h
-    scale = dim_head ** -0.5
+        out = attention_function(q, k, v, attn.heads, mask)
+        return attn.to_out(out)
 
-    q, k, v = map(lambda t: atm.rearrange(t, 'b n (h d) -> (b h) n d', h=h), (q, k, v))
-    sim = atm.einsum('b i d, b j d -> b i j', q, k) * scale
 
-    if self.active:
-        if tokens:
-            for token in tokens:
-                start = (v.shape[1]//77 - len(tokens)) * 77
-                #print("v.shape:",v.shape,"start:",start+1,"stop:",start+token)
-                v[:,start+1:start+token,:] = -v[:,start+1:start+token,:]
+def attention_function(q, k, v, heads, mask=None, attn_precision=None, skip_reshape=False):
+    if skip_reshape:
+        b, _, _, dim_head = q.shape
+    else:
+        b, _, dim_head = q.shape
+        dim_head //= heads
+        q, k, v = map(
+            lambda t: t.view(b, -1, heads, dim_head).transpose(1, 2),
+            (q, k, v),
+        )
 
-    if atm.exists(mask):
-        mask = atm.rearrange(mask, 'b ... -> b (...)')
-        max_neg_value = -torch.finfo(sim.dtype).max
-        mask = atm.repeat(mask, 'b j -> (b h) () j', h=h)
-        sim.masked_fill_(~mask, max_neg_value)
-
-    attn = sim.softmax(dim=-1)
-    #print(h,context.shape,q.shape,k.shape,v.shape,attn.shape)
-    out = atm.einsum('b i j, b j d -> b i d', attn, v)
-
-    out = atm.rearrange(out, '(b h) n d -> b n (h d)', h=h)
-
-    return module.to_out(out)
-
-import inspect
+    out = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=mask, dropout_p=0.0, is_causal=False)
+    out = (
+        out.transpose(1, 2).reshape(b, -1, heads * dim_head)
+    )
+    return out
 
 def hook_forwards(self, root_module: torch.nn.Module, remove=False):
     for name, module in root_module.named_modules():
@@ -447,6 +408,7 @@ def hook_forwards(self, root_module: torch.nn.Module, remove=False):
             module.forward = hook_forward(self, module)
             if remove:
                 del module.forward
+
 
 def resetpcache(p):
     p.cached_c = [None,None]
